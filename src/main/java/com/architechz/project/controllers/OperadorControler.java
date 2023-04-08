519c1406048dac0cdc81956310e0aa79fb91d1ad
Gabriel De Souza <gabriel.desouza@javeriana.edu.co>
2023-04-07 22:28:04 -0500

    Current changes
    CRUD changes being made and upload to the github

--- src/main/java/com/architechz/project/controllers/OperadorControler.java
+++ src/main/java/com/architechz/project/controllers/OperadorControler.java
@@ -12,10 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.architechz.project.dto.DtoLinkOperatorManager;
-import com.architechz.project.dto.DtoOperator;
-import com.architechz.project.models.Operador;
 import com.architechz.project.service.Operadores.OperadoresService;
 
 
@@ -23,43 +19,6 @@ import com.architechz.project.service.Operadores.OperadoresService;
 @RestController
 @RequestMapping("/api/opt")
 public class OperadorControler {
-    
-    @Autowired
-    OperadoresService operadoresService;
 
-    @GetMapping("/operator/{id}")
-    public Operador getOperator(@PathVariable("id") Long id){
-        return operadoresService.getOperatorById(id);
-    }
-
-    @GetMapping("/allOperators")
-    public List<Operador> getAllOperators() {
-        return operadoresService.getAllOperators();
-    }
-
-    @GetMapping("/infoOperators")
-    public List<DtoOperator> getInfoOperators() {
-        return operadoresService.getInfoOperators();
-    }
-
-    @PostMapping("/operator")
-    public void createOperator(@RequestBody Operador operator) {
-        operadoresService.createOperator(operator);
-    }
-
-    @DeleteMapping("/operator/{id}")
-    public void deleteOperator(@PathVariable("id") Long id){
-        operadoresService.deleteOperator(id);
-    }
-
-    @PutMapping("/linkOperatorManager")
-    public void linkOperatorManager(@RequestBody DtoLinkOperatorManager dtoLinkOperatorManager){
-        operadoresService.linkOperatorManager(dtoLinkOperatorManager);
-    }
-
-    @PutMapping("/unlinkOperatorManager")
-    public void unlinkOperatorManager(@RequestBody DtoLinkOperatorManager dtoLinkOperatorManager){
-        operadoresService.unlinkOperatorManager(dtoLinkOperatorManager);
-    }
 
 }
