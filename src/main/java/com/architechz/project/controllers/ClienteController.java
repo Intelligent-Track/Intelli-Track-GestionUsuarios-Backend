527ea931b9b65bee8efb189e1d18f8f34ce37f80
Santiago Fernandez <sa.fernandez73@gmail.com>
2023-04-14 21:37:32 -0500

    Update client added

--- src/main/java/com/architechz/project/controllers/ClienteController.java
+++ src/main/java/com/architechz/project/controllers/ClienteController.java
@@ -1,12 +1,30 @@
 package com.architechz.project.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.architechz.project.models.Client;
+import com.architechz.project.payload.response.MessageResponse;
+import com.architechz.project.service.Clientes.ClienteService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/cli")
 public class ClienteController {
+
+    @Autowired
+    ClienteService clienteService;
+
+    @PutMapping("/updateClient")
+    public ResponseEntity<?> UpdateClient(@Valid @RequestBody Client clientRequest) {
+      return ResponseEntity.ok(new MessageResponse(this.clienteService.updateClient(clientRequest)));
+    }
     
 }
--- src/main/java/com/architechz/project/service/Clientes/ClienteServiceImpl.java
+++ src/main/java/com/architechz/project/service/Clientes/ClienteServiceImpl.java
@@ -4,23 +4,18 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.architechz.project.payload.RegisterRequests.ClienteRequest;
-
 import com.architechz.project.models.Client;
-
-import com.architechz.project.service.AuthService.*;
+import com.architechz.project.payload.InsertionRequests.ClienteRequest;
 import com.architechz.project.payload.request.SignupRequest;
 import com.architechz.project.repository.ClienteRepository;
 import com.architechz.project.repository.UserRepository;
+import com.architechz.project.service.AuthService.AuthService;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
-
 
     @Autowired
     AuthService AuthService;
@@ -35,61 +30,83 @@ public class ClienteServiceImpl implements ClienteService {
     public String addUser(ClienteRequest user) {
         if (ClienteRepository.existsByUsername(user.getUsername())) {
 
-            return "Error: El correo "+ user.getUsername() + " ya existe en nuestras bases de datos!";
-          }
-          else{
+            return "Error: El correo " + user.getUsername() + " ya existe en nuestras bases de datos!";
+        } else {
 
             if (ClienteRepository.existsByNit(user.getNit())) {
 
-                return "Error: El Nit "+ user.getNit() + " ya existe en nuestras bases de datos!";
-              }
-              else{
+                return "Error: El Nit " + user.getNit() + " ya existe en nuestras bases de datos!";
+            } else {
 
-        Set<String>rol = new HashSet<>();
-        
-        try {
+                Set<String> rol = new HashSet<>();
 
-            if(user.getAdm()){
+                try {
 
-                Client cliente = new Client(user.getName(), user.getUsername(), user.getDocument(), user.getPhone(), "Cliente ADM", user.getLocation(), user.getNit(), user.getCompanyName(), user.getAdm(), user.getManagerUsername());
-                ClienteRepository.save(cliente);
-            }else{
-                rol.add("CLI");
-                Client cliente = new Client(user.getName(), user.getUsername(), user.getDocument(), user.getPhone(), "Cliente ADM", user.getLocation(), user.getNit(), user.getCompanyName(), user.getAdm(), user.getManagerUsername());
-                ClienteRepository.save(cliente);
+                    if (user.getAdm()) {
+
+                        Client cliente = new Client(user.getName(), user.getUsername(), user.getDocument(),
+                                user.getPhone(), "Cliente ADM", user.getLocation(), user.getNit(),
+                                user.getCompanyName(), user.getAdm(), user.getManagerUsername());
+                        ClienteRepository.save(cliente);
+                    } else {
+                        rol.add("CLI");
+                        Client cliente = new Client(user.getName(), user.getUsername(), user.getDocument(),
+                                user.getPhone(), "Cliente ADM", user.getLocation(), user.getNit(),
+                                user.getCompanyName(), user.getAdm(), user.getManagerUsername());
+                        ClienteRepository.save(cliente);
+                    }
+
+                    SignupRequest user2 = new SignupRequest(user.getName(), user.getUsername(), user.getPassword(),
+                            rol);
+                    AuthService.addUser(user2);
+
+                } catch (Exception e) {
+                    return e.toString();
+                }
+
+                return "Gerente guardado con exito";
             }
-            
-
-            SignupRequest user2 = new SignupRequest(user.getName(), user.getUsername(), user.getPassword(), rol );
-            AuthService.addUser(user2);
-    
-            } catch (Exception e) {
-                return e.toString(); 
         }
-    
-            return "Gerente guardado con exito";
     }
-    }
-}
 
     @Override
     public List<Client> GetUser() {
         return ClienteRepository.findAll();
     }
 
-    @Transactional
+    @Override
     public String delUser(String username) {
         try {
-            
+
             ClienteRepository.deleteByUsername(username);
             UserRepository.deleteByUsername(username);
-
 
         } catch (Exception e) {
             return e.toString();// TODO: handle exception
         }
 
-
-        return "Cliente " + username +" borrado con exito!";
+        return "Cliente " + username + " borrado con exito!";
     }
+
+    @Override
+    public String updateClient(Client client) {
+        try {
+            Client clientFound = this.ClienteRepository.findById(client.getId()).orElseThrow();
+            clientFound.setAdm(client.getAdm());
+            clientFound.setCompanyName(client.getCompanyName());
+            clientFound.setDocument(client.getDocument());
+            clientFound.setJob(client.getJob());
+            clientFound.setLocation(client.getLocation());
+            clientFound.setManagerUsername(client.getManagerUsername());
+            clientFound.setName(client.getName());
+            clientFound.setNit(client.getNit());
+            clientFound.setPhone(client.getPhone());
+            clientFound.setUsername(client.getUsername());
+            this.ClienteRepository.save(clientFound);
+            return "Cliente actualizado correctamente";
+        } catch (Exception e) {
+            return e.toString();
+        }
+    }
+
 }
