f8b722a331fd3da00bf240b655a5ea83f16bc2fb
JoseTorres34 <torresjose.ap@gmail.com>
2023-04-29 22:33:49 -0500

    Clientes y EmailNotifications editados

--- src/main/java/com/architechz/project/service/Clientes/ClienteServiceImpl.java
+++ src/main/java/com/architechz/project/service/Clientes/ClienteServiceImpl.java
@@ -12,12 +12,16 @@ import com.architechz.project.payload.RegisterRequests.ClienteRequest;
 import com.architechz.project.repository.ClienteRepository;
 import com.architechz.project.repository.UserRepository;
 import com.architechz.project.service.AuthService.AuthService;
+import com.architechz.project.service.EmailNotifications.EmailService;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
     @Autowired
     AuthService AuthService;
+
+    @Autowired
+    EmailService emailService;
 
     @Autowired
     ClienteRepository clienteRepository;
@@ -27,6 +31,7 @@ public class ClienteServiceImpl implements ClienteService {
 
     @Override
     public String addUser(ClienteRequest user) {
+        String messagge;
         if (this.clienteRepository.existsByUsername(user.getUsername())) {
 
             return "Error: El correo " + user.getUsername() + " ya existe en nuestras bases de datos!";
@@ -34,7 +39,26 @@ public class ClienteServiceImpl implements ClienteService {
 
             if (this.clienteRepository.existsByNit(user.getNit())) {
 
-                return "Error: El Nit " + user.getNit() + " ya existe en nuestras bases de datos!";
+                Set<String> rol = new HashSet<>();
+                rol.add("CLI");
+                Client cliente = new Client(
+                    user.getName(), 
+                    user.getUsername(), 
+                    user.getDocument(), 
+                    user.getPhone(),
+                    "Cliente Representante", 
+                    user.getLocation(), 
+                    user.getNit(), 
+                    user.getCompanyName(),
+                    user.getAdm(), 
+                    user.getManagerUsername()
+                );
+
+                clienteRepository.save(cliente);
+                messagge= "Bienvenido, usted a sido registrado como un Cliente representante de la empresa" + user.getCompanyName();
+                emailService.sentMessagge(user.getUsername(),messagge);
+    
+                    return "El usuario con correo " + user.getUsername() + " fue añadido como Cliente Representante exitosamente!";
             } else {
 
                 Set<String> rol = new HashSet<>();
